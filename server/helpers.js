67ca4abe01c1e9acbbd15847900448b03f83a859
GrahamPerich <graham.perich@gmail.com>
2017-01-04 16:06:42 -0800

    more server work

--- server/helpers.js
+++ server/helpers.js
@@ -1,3 +1,7 @@
+var neo4j = require('neo4j-driver').v1;
+var driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', '12345'));
+var session = driver.session();
+
 var userDoesExist = function(userID) {
   session.run (
       'MATCH (u:User) \
