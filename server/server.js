35888b96a17acc7d7b9a298dbc5e49af067e36c5
GrahamPerich <graham.perich@gmail.com>
2017-01-07 10:47:10 -0800

    uncomment AWS code

--- server/server.js
+++ server/server.js
@@ -15,28 +15,28 @@ var expressJWT = require('express-jwt');
 var jwt = require('jsonwebtoken');
 
 // .env access
-// require('dotenv').config();
+require('dotenv').config();
 
-// // initialize aws s3 
-// const s3 = new aws.S3({
-//   accessKeyId: process.env.AWS_ACCESS_ID,
-//   secretAccessKey: process.env.AWS_ACCESS_KEY,
-// });
+// initialize aws s3 
+const s3 = new aws.S3({
+  accessKeyId: process.env.AWS_ACCESS_ID,
+  secretAccessKey: process.env.AWS_ACCESS_KEY,
+});
 
-// // Initialize multers3 with our s3 config and other options
-// const upload = multer({
-//   storage: multerS3({
-//     s3,
-//     bucket: 'sharemap',
-//     acl: 'public-read',
-//     metadata(req, file, cb) {
-//       cb(null, {fieldName: file.fieldname});
-//     },
-//     key(req, file, cb) {
-//       cb(null, Date.now().toString() + '.png');
-//     }
-//   })
-// });
+// Initialize multers3 with our s3 config and other options
+const upload = multer({
+  storage: multerS3({
+    s3,
+    bucket: 'sharemap',
+    acl: 'public-read',
+    metadata(req, file, cb) {
+      cb(null, {fieldName: file.fieldname});
+    },
+    key(req, file, cb) {
+      cb(null, Date.now().toString() + '.png');
+    }
+  })
+});
 
 // START SERVER; CONNECT DATABASE
 var app = express();
