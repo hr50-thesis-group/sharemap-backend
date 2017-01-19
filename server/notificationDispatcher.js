1e7dbd424e3f35ebc03a32529234ef6900e261ca
GrahamPerich <graham.perich@gmail.com>
2017-01-19 14:31:10 -0800

    add a check to see if a push notification has been sent to a certain ID to prevent duplicate notifications

--- server/notificationDispatcher.js
+++ server/notificationDispatcher.js
@@ -8,17 +8,22 @@ var sdk = require('exponent-server-sdk');
 
 module.exports = {
   sendPushNotification: (data) => {
+    var alreadySent = {};
     var userFirstName = data.records[0]._fields[1];
     userFirstName = userFirstName.slice(0, 1).toUpperCase() + userFirstName.slice(1);
 
     data.records.forEach(record => {
-      var token = record._fields[0];
-      if(sdk.isExponentPushToken(token)) {
-        sdk.sendPushNotificationAsync({
-          exponentPushToken: token, // The push token for the app user you want to send the notification to
-          message: userFirstName + ' just posted a pin near you!',
-          data: {message: userFirstName + ' just posted a pin near you!'}
-        });
+
+      if (!alreadySent[token]) {
+        var token = record._fields[0];
+        alreadySent[token] = true;
+        if(sdk.isExponentPushToken(token)) {
+          sdk.sendPushNotificationAsync({
+            exponentPushToken: token, // The push token for the app user you want to send the notification to
+            message: userFirstName + ' just posted a pin near you!',
+            data: {message: userFirstName + ' just posted a pin near you!'}
+          });
+        }
       }
     })
   }
